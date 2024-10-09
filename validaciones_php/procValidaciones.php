aa3f86e3fd8f0a3f132fe5a0a0aa33d6b40a8fdf
AdriMartin <adrianmartincalvet@gmail.com>
2024-10-09 17:12:28 +0200

    Archivos de validaciones creados

---
+++ validaciones_php/procValidaciones.php
@@ -0,0 +1,24 @@
+<?php
+/*  
+---- VALIDA USERNAME ----
+- Que el campo no esté vacío
+- Que el campo no tenga menos de 3 cc
+- Que el campo no sea un numero
+- No debe contener cc especiales
+- Si todo OK se limpia el campo
+*/
+
+
+
+
+/*
+---- VALIDA PASSWORD ----
+- Que el campo no esté vacío
+- Que la contraseña tenga min 8cc
+- Que contenga numeros y letras
+-- Si todo OK se limpia
+*/
+
+/*
+---- SANEAR ----
+*/
