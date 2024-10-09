1dd040b5528a8a6a074aa60ee8283b98981c8418
AdriMartin <adrianmartincalvet@gmail.com>
2024-10-09 18:27:46 +0200

    Index con eventos de validaciones, Clase error añadida a CSS, Validación JS a medias, Validación php sin empezar

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,27 @@
 - No debe contener cc especiales
 - Si todo OK se limpia el campo
 */
+function ValidaUserName(){
+    let username = document.getElementById('usuario').value;
+    let errorUser = document.getElementById('error_name');
+
+    if(username === ""){
+        errorUser.innerHTML = "El campo no tiene que estar vacío";
+        return false;
+    } else if(username.length < 3){
+        errorUser.innerHTML = "El campo tiene que tener más de 3 carácteres";
+        return false;
+    } else if(!isNaN(username)){
+        errorUser.innerHTML = "El campo no tiene que ser un número";
+        return false;
+    } else if(username.includes(/[.*+?^${}()|[\]\\]/g, '\\$&')){
+        errorUser.innerHTML = "El campo no tiene que tener carácteres especiales";
+        return false;
+    } else {
+        errorUser = "";
+        return true;
+    }
+}
 
 
 
